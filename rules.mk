780d7a3e2d33bbd9dbf267136959613e4314c9e6
digitalsunshine <indra.ariyadi@gmail.com>
2026-06-01 10:49:09 +0700

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -15,6 +15,7 @@ BOOTLOADER = halfkay
 # Build Options
 #   change yes to no to disable
 #
+COMBO_ENABLE = yes    
 BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
